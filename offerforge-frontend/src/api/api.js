2c6eccdee63e3c7c5205c65fecf2d18a5092e44b
Renuka-Ratnala <deviratnala13@gmail.com>
2026-08-08 21:23:46 +0530

    Connecting frontend and backend

--- offerforge-frontend/src/api/api.js
+++ offerforge-frontend/src/api/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://offerforge-backend-rxyb.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
